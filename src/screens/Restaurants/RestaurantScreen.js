527aaa1ee097f1579a20208bd2919f4703ffd837
Francisco Alvarez <faovenezuela@gmain.com>
2024-04-11 17:49:41 -0300

    Navigation Button Screen

--- src/screens/Restaurants/RestaurantScreen.js
+++ src/screens/Restaurants/RestaurantScreen.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { Button } from '@rneui/themed'
+import { screen } from '../../utils'
 
+export function RestaurantScreen(props) {
+    const { navigation } = props;
 
-export function RestaurantScreen() {
+    const gotoRestaurant = () => {
+        //navigation.navigate(screen.restaurant.addRestaurant)
+        //console.log(navigation.navigate)
+        navigation.navigate(screen.account.tab, screen.account.account)
+    }
+
     return (
         <View>
             <Text>Estamos en RestaurantScreen</Text>
+            <Button title="Crear Restaurante" onPress={gotoRestaurant}/>
         </View>       
     )
 }
