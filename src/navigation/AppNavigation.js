4883e3d551f2faae5111dfe900407e16eb986cd6
Francisco Alvarez <faovenezuela@gmain.com>
2024-04-11 09:14:10 -0300

    Subiendo iconos

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Icon } from "@rneui/themed";
 import { RestaurantScreen } from "../screens/RestaurantScreen";
 import { FavoritesScreen } from "../screens/FavoritesScreen";
 import { RankingScreen } from "../screens/RankingScreen";
@@ -10,7 +11,11 @@ const Tab = createBottomTabNavigator();
 
 export function AppNavigation() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            tabBarActiveTintColor: "#00a680",
+            tabBarInactiveTintColor: "#646464",
+            tabBarIcon: ({ color, size }) => screenOptions(route, color, size),
+        })}>
             <Tab.Screen name="Restaurant" component={RestaurantScreen} />
             <Tab.Screen name="Favorites" component={FavoritesScreen} />
             <Tab.Screen name="Ranking" component={RankingScreen} />
@@ -19,3 +24,34 @@ export function AppNavigation() {
         </Tab.Navigator>
     )
 }
+
+function screenOptions(route, color, size) {
+    let iconName;
+
+    if (route.name === "Restaurant") {
+        iconName = "compass-outline";
+    }
+
+    if (route.name === "Favorites") {
+        iconName = "heart-outline";
+    }
+
+    if (route.name === "Ranking") {
+        iconName = "star-outline";
+    }
+
+    if (route.name === "Search") {
+        iconName = "magnify";
+    }
+
+    if (route.name === "Account") {
+        iconName = "home-outline";
+    }
+
+    return (
+        <Icon type="material-community"
+                name={iconName} color={color} size={size}/>
+    )
+}
+
+
