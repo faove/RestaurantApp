b480a9eaf588f30e520c860d75b3b2a4a858db85
Francisco Alvarez <faovenezuela@gmain.com>
2024-04-12 10:35:02 -0300

    UserGuest and UserLogin

---
+++ src/screens/Account/AccountScreen.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { UserGuestScreen } from "./UserGuestScreen";
+import { UserLoggedScreen } from "./UserLoggedScreen";
+
+export function AccountScreen() {
+    const [hasLogged, setHasLogged] = useState(null);
+
+    useEffect( () => {
+        const auth = getAuth();
+
+        onAuthStateChanged(auth, (user) => {
+            setHasLogged(user ? true : false);
+        });
+
+    },[])
+
+    return hasLogged ? <UserLoggedScreen/> : <UserGuestScreen/>; 
+}
