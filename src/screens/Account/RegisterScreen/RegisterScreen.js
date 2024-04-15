7a12b44f0d6f39341657073014ccda57eae74817
Francisco Alvarez <faovenezuela@gmain.com>
2024-04-15 14:12:15 -0300

    Adding react-native-keyboard-aware-scroll-view

--- src/screens/Account/RegisterScreen/RegisterScreen.js
+++ src/screens/Account/RegisterScreen/RegisterScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Image } from '@rneui/base'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { RegisterForm } from "../../../components/Auth"
 import { styles } from "../RegisterScreen/RegisterScreen.styles";
 
 export function RegisterScreen() {
   return (
-    <View>
+    <KeyboardAwareScrollView>
       <Image 
         source={require('../../../../assets/img/restaurantapp.png')}
         style={styles.image}
@@ -14,6 +15,6 @@ export function RegisterScreen() {
       <View style={styles.content}>
         <RegisterForm />
       </View>  
-    </View>
+    </KeyboardAwareScrollView>
   )
 }
