6198c26a67811c12ebde14fbde0aea1612d1c2c4
falvarezortiz <falvarez@southhive.com>
2024-08-14 15:23:52 -0300

    Mostrar imagenes subidas

--- src/components/Restaurants/AddRestaurant/UploadImagesForm/UploadImagesForm.js
+++ src/components/Restaurants/AddRestaurant/UploadImagesForm/UploadImagesForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, Alert } from 'react-native'
+import { ScrollView, Alert } from 'react-native'
 import { Icon, Avatar, Text } from "@rneui/base"
 import * as ImagePicker from "expo-image-picker";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
@@ -60,7 +60,7 @@ export function UploadImagesForm(props) {
 
   return (
     <>
-      <View style={styles.viewImage}>
+      <ScrollView style={styles.viewImage} horizontal showsHorizontalScrollIndicator={false}>
         <Icon 
             type= "material-community"
             name= "camera"
@@ -75,7 +75,7 @@ export function UploadImagesForm(props) {
             containerStyle={styles.imageStyle}
           />
         ))}
-      </View>
+      </ScrollView>
       <Text style={styles.error}>{formik.errors.images}</Text>
       <LoadingModal show={isLoading} text="Subiendo imagen" /> 
     </>
